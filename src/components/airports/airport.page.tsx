e2a56c03a3e41633c7f71666bc18a5713c873ae4
Fabio Fognani <fabio.fognani+gitlab@gmail.com>
2022-05-11 07:55:35 +0200

    feature(airport): added flights arriving in and departing from single airport;

--- src/components/airports/airport.page.tsx
+++ src/components/airports/airport.page.tsx
@@ -10,9 +10,17 @@ import { airportsSelector } from './airports.slice';
 import { DEFAULT_MAP_ZOOM } from '../../utils/constants';
 import { MarkerIcon } from '../common/icons';
 import { AirportDetails } from './airport-details';
+import { FlightItem } from '../flights/flight-item';
+import { airlinesSelector } from '../airlines/airlines.slice';
+import { allFlightsSelector } from '../flights/flights.slice';
+import { enrighFlightWithDetails } from '../../utils/flights';
+import { FlightPathItem } from '../flights/flights.types';
 
 export function Airport() {
+  const airlines = useSelector(airlinesSelector);
   const airports = useSelector(airportsSelector);
+  const flights = useSelector(allFlightsSelector);
+
   const { airportId } = useParams();
 
   const airport = useMemo(() => {
@@ -24,6 +32,18 @@ export function Airport() {
     +(airport?.longitude || 0),
   ];
 
+  const airportFullFlightsDeparting = useMemo(() => {
+    return flights.filter(f => f.departureAirportId.toString() === airportId)
+      .map(f => enrighFlightWithDetails(f, airlines, airports))
+      .filter(Boolean) as FlightPathItem[];
+  }, [airportId, airlines, airports, flights]);
+
+  const airportFullFlightsArriving = useMemo(() => {
+    return flights.filter(f => f.arrivalAirportId.toString() === airportId)
+      .map(f => enrighFlightWithDetails(f, airlines, airports))
+      .filter(Boolean) as FlightPathItem[];
+  }, [airportId, airlines, airports, flights]);
+
   return <PageSection className="p-8">
     <div>
       <Typography variant="h3" className="text-neutral">
@@ -34,28 +54,55 @@ export function Airport() {
         <AirportDetails airport={airport} className="mb-4" />
       )}
 
-      <MapContainer
-        style={{ width: '100%', height: '60vh' }}
-        center={airportCoordinates}
-        zoom={DEFAULT_MAP_ZOOM}
-        scrollWheelZoom
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          position={airportCoordinates}
-          icon={MarkerIcon}
+      <div className="mb-8">
+        <MapContainer
+          style={{ width: '100%', height: '60vh' }}
+          center={airportCoordinates}
+          zoom={DEFAULT_MAP_ZOOM}
+          scrollWheelZoom={false}
         >
-          <Popup>
-            <strong>{airport?.codeIata}</strong>
-            {airport && (
-              <AirportDetails airport={airport} />
-            )}
-          </Popup>
-        </Marker>
-      </MapContainer>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={airportCoordinates}
+            icon={MarkerIcon}
+          >
+            <Popup>
+              <strong>{airport?.codeIata}</strong>
+              {airport && (
+                <AirportDetails airport={airport} />
+              )}
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+
+      <div className="flex flex-col sm:flex-row space-x-8 sm:justify-evenly">
+        <div>
+          <Typography variant="h4" className="mt-3 text-neutral">
+            Flights departing
+          </Typography>
+          {
+            airportFullFlightsDeparting.map(flight => {
+              return <FlightItem key={flight.id} flight={flight} divider />;
+            })
+          }
+        </div>
+
+        <div>
+          <Typography variant="h4" className="mt-3 text-neutral">
+            Flights arriving
+          </Typography>
+          {
+            airportFullFlightsArriving.map(flight => {
+              return <FlightItem key={flight.id} flight={flight} divider />;
+            })
+          }
+        </div>
+      </div>
+
     </div>
   </PageSection>;
 }
